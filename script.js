b361daea09ce6dba5d792ed5424238c9bf328b56
rschwenck <rodrigo.schwenck@gmail.com>
2023-07-05 10:29:39 -0300

    Site - Aplicação Web
    Site da previsão do tempo da cidade de São Paulo criado para rodar em um container apache.

---
+++ script.js
@@ -0,0 +1,36 @@
+﻿window.addEventListener('DOMContentLoaded', getWeather);
+
+function getWeather() {
+  // Substitua 'YOUR_API_KEY' pela sua chave de API do OpenWeatherMap
+  const apiKey = '6b0271ab6bbee1b6d15c52f75497461b';
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=São Paulo&appid=${apiKey}&units=metric`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const weatherContainer = document.getElementById('weather-container');
+      weatherContainer.innerHTML = '';
+
+      const city = data.name;
+      const temperature = data.main.temp;
+      const description = data.weather[0].description;
+
+      const cityElement = document.createElement('h3');
+      cityElement.textContent = `Cidade: ${city}`;
+
+      const temperatureElement = document.createElement('p');
+      temperatureElement.textContent = `Temperatura: ${temperature} °C`;
+
+      const descriptionElement = document.createElement('p');
+      descriptionElement.textContent = `Descrição: ${description}`;
+
+      weatherContainer.appendChild(cityElement);
+      weatherContainer.appendChild(temperatureElement);
+      weatherContainer.appendChild(descriptionElement);
+    })
+    .catch(error => {
+      console.log('Ocorreu um erro:', error);
+      const weatherContainer = document.getElementById('weather-container');
+      weatherContainer.innerHTML = '<h3>Erro ao obter a previsão do tempo.</h3>';
+    });
+}
